9d9346757fbf70db506085a0e5d841163c56b6cb
Tero Koponen <terokoponen@kamk.fi>
2024-08-17 23:06:17 +0300

    Fix missing meshes on interact

--- OnlyDown/src/Interactable.gd
+++ OnlyDown/src/Interactable.gd
@@ -3,9 +3,12 @@ extends Node3D
 @onready var interaction_area: InteractionArea = $InteractionArea
 @onready var player = get_tree().get_first_node_in_group("player")
 
+@export var mesh: Mesh
+
 func _ready():
+	$Mesh.mesh = mesh
 	interaction_area.interact = Callable(self, "_on_interact")
 
 # TODO: make it so it falls on the ground and loses ability to interact??
 func _on_interact():
-	player.set_mesh.emit($Mesh.mesh)
+	player.set_mesh.emit(mesh)
